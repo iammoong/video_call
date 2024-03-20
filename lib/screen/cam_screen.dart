8db55cae8e87f40584f4e57812197e8cbecf1068
iammoong <ansrlwkd225@naver.com>
2024-03-20 23:27:43 +0900

    화상 채팅 중간까지 완성

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,8 +1,22 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:video_call/const/agora.dart';
 
-class CamScreen extends StatelessWidget {
+class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
+
+  @override
+  State<CamScreen> createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+
+  // 내 ID
+  int? uid = 0;
+
+  int? otherUid;
 
   @override
   Widget build(BuildContext context) {
@@ -13,49 +27,107 @@ class CamScreen extends StatelessWidget {
         ),
       ),
       body: FutureBuilder<bool>(
-        future: init(),
-        builder: (context, snapshot) {
-          if(snapshot.hasError) {
-            return Center(
-             child: Text(
-               snapshot.error.toString(),
-             ),
+          future: init(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                ),
+              );
+            }
+
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Column(
+              children: [],
             );
-          }
-
-          if(!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          return Column(
-            children: [
-
-            ],
-          );
-        }
-      ),
+          }),
     );
   }
 
+  renderMainView(){
+
+  }
+
   Future<bool> init() async {
-    final resp = await[Permission.camera, Permission.microphone].request();
+    final resp = await [Permission.camera, Permission.microphone].request();
 
     final cameraPermission = resp[Permission.camera];
     final microphonePermission = resp[Permission.microphone];
 
-    if(cameraPermission != PermissionStatus.granted) {
+    if (cameraPermission != PermissionStatus.granted) {
       throw '카메라 권한이 없습니다.';
     }
 
-    if(microphonePermission != PermissionStatus.granted) {
+    if (microphonePermission != PermissionStatus.granted) {
       throw '마이크 권한이 없습니다.';
     }
 
-    
+    if (engine == null) {
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(
+        RtcEngineContext(
+          appId: APP_ID,
+        ),
+      );
+
+      engine!.registerEventHandler(
+        RtcEngineEventHandler(
+          //내가 채널에 입장했을 때
+          // connection -> 연결정보
+          // elapsed -> 연결된 시간
+          onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+            print('채널에 입장했습니다. uid :  ${connection.localUid}');
+            setState(() {
+              uid = connection.localUid;
+            });
+          },
+          //내가 채널에 나갔을 때
+          onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+            print('채널 퇴장');
+            setState(() {
+              uid = null;
+            });
+          },
+          // 상대방 유저가 들어왔을 때
+          onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+            print('상대가 채널에 입장했습니다. otherUid : $remoteUid');
+
+            setState(() {
+              otherUid = remoteUid;
+            });
+          },
+          // 상대가 나갔을 때
+          onUserOffline: (RtcConnection connection, int remoteUid,
+              UserOfflineReasonType reason) {
+            print('상대가 채널에서 나갔습니다. otherUid : $remoteUid');
+
+            setState(() {
+              otherUid = null;
+            });
+          },
+        ),
+      );
+    }
+
+    await engine!.enableVideo();
+
+    await engine!.startPreview();
+
+    ChannelMediaOptions options = ChannelMediaOptions();
+
+    await engine!.joinChannel(
+        token: TEMP_TOKEN,
+        channelId: CHANNEL_NAME,
+        uid: 0,
+        options: options);
 
     return true;
   }
 }
-
